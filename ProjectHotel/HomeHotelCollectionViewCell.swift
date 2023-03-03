fac94428ea1dd186ebbd2e174a83502ff64e5bfd
Iqlima Rahmawati <80628223+iqlimarahmawati@users.noreply.github.com>
2023-03-03 15:04:09 +0700

    Add files via upload

---
+++ ProjectHotel/HomeHotelCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  HomeHotelCollectionViewCell.swift
+//  ProjectHotel
+//
+//  Created by Phincon on 02/03/23.
+//
+
+import UIKit
+
+class HomeHotelCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
